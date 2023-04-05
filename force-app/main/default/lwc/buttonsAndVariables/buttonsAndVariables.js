fa33c4de7c3f8d17357d47b2e1b3f9d42c5ee605
Sai Vinay <sai.vinay@silverlinecrm.com>
2023-04-05 15:30:27 +0530

    buttonsAndVariables

---
+++ force-app/main/default/lwc/buttonsAndVariables/buttonsAndVariables.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class ButtonsAndVariables extends LightningElement {
+
+    customerCode = 'C12345';
+    customerName = 'Sai Vinay';
+    customerEmail = 'sai@gmail.com';
+    balanceAmount = '2222';
+    companyName = 'TATA';
+    customerAddress = 'Gajapathinagaram';
+}
